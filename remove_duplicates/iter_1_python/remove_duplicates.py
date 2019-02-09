98087935e057b7616c4dac6f2ab408db5a5c150b
elalaouifaris <elalaouifaris@gmail.com>
2019-02-09 19:15:05 +0100

    refactor remove duplicates list comprehension for readability

--- remove_duplicates/iter_1_python/remove_duplicates.py
+++ remove_duplicates/iter_1_python/remove_duplicates.py
@@ -1,5 +1,6 @@
 def remove_duplicates(duplicates_list):
-  no_duplicates = []
-  [no_duplicates.append(element) for element in duplicates_list
-    if element not in no_duplicates]
-  return no_duplicates
+  unique = []
+  [unique.append(element) for element in duplicates_list
+    if element not in unique]
+
+  return unique
