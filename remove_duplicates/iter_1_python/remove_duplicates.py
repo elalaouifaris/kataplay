1985c0bfc943fe1e9958989af3078f41cece9821
elalaouifaris <elalaouifaris@gmail.com>
2019-02-09 19:05:56 +0100

    Implement remove_duplicates with list comprehesion

--- remove_duplicates/iter_1_python/remove_duplicates.py
+++ remove_duplicates/iter_1_python/remove_duplicates.py
@@ -1,6 +1,5 @@
 def remove_duplicates(duplicates_list):
   no_duplicates = []
-  for element in duplicates_list:
-    if element not in no_duplicates:
-      no_duplicates.append(element)
+  [no_duplicates.append(element) for element in duplicates_list
+    if element not in no_duplicates]
   return no_duplicates
