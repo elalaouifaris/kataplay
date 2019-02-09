c1c07e2f65548bdf8589c6f0717343df778ae9d6
elalaouifaris <elalaouifaris@gmail.com>
2019-02-09 18:50:59 +0100

    Refactor tests to includes longer lists examples preserving order

--- remove_duplicates/iter_1_python/test_remove_duplicates.py
+++ remove_duplicates/iter_1_python/test_remove_duplicates.py
@@ -3,8 +3,11 @@ from remove_duplicates import remove_duplicates
 def test_remove_duplicates_from_empty_list_returns_empty_list():
   assert remove_duplicates([]) == []
 
-def test_remove_duplicates_from_1_element_list_return_same_list():
-  assert remove_duplicates([1]) == [1]
-
 def test_remove_duplicates_from_2_same_element_list_returns_1_element_list():
   assert remove_duplicates([1, 1]) == [1]
+
+def test_remove_duplicates_from_no_duplicates_list_returns_same_list():
+  assert remove_duplicates([1, 3, 5]) == [1, 3, 5]
+
+def test_remove_duplicates_from__duplicates_list_returns_list_without_duplicates():
+  assert remove_duplicates([1, 1, 5, 3, 5]) == [1, 5, 3]
