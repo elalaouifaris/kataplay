030514705022fa485fc587641abe4b10ab1342ca
elalaouifaris <elalaouifaris@gmail.com>
2019-07-15 13:49:49 +0200

    Implement the minute with seconds formating

--- iter_1_elixir/duration_formater.ex
+++ iter_1_elixir/duration_formater.ex
@@ -1,10 +1,48 @@
 defmodule DurationFormater do
-  @duration_name %{
+  @label %{
+    unit: "1",
+    plural: "s",
+    and:  " and ",
     now: "now",
-    second: "second"
+    second: "second",
+    minute: "minute"
   }
 
-  def format_duration(0), do: @duration_name.now
-  def format_duration(1), do: "1 #{@duration_name.second}"
-  def format_duration(seconds), do: "#{seconds} #{@duration_name.second}s"
+  @in_seconds %{
+    minute: 60
+  }
+
+  def format_duration(0), do: @label.now
+  def format_duration(seconds) do
+    decompose_duration(seconds)
+    |> format_output()
+  end
+
+  defp decompose_duration(seconds) do
+    minute_part = div(seconds, @in_seconds.minute) 
+    second_part = rem(seconds, @in_seconds.minute)
+
+    cond do
+      seconds == 0 ->
+        []
+
+      seconds == 1 ->
+        [{@label.unit, @label.second}]
+
+      seconds == @in_seconds.minute ->
+        [{@label.unit, @label.minute} | decompose_duration(second_part)]
+
+      seconds > @in_seconds.minute ->
+        [{minute_part, @label.minute, @label.plural} | decompose_duration(second_part)]
+
+      true ->
+        [{seconds, @label.second, @label.plural}]
+    end
+  end
+
+  defp format_output([before_last, last | []]) do
+    format_output([before_last]) <> @label.and <> format_output([last])
+  end
+  defp format_output([{count, label}| []]), do: "#{count} #{label}"
+  defp format_output([{count, label, plural}| []]), do: "#{count} #{label}#{plural}"
 end
