a5cc291f0a2d23755e6f80faa67190b6a0ce26cc
elalaouifaris <elalaouifaris@gmail.com>
2019-07-15 12:35:58 +0200

    Implement duration formater for seconds

--- iter_1_elixir/duration_formater.ex
+++ iter_1_elixir/duration_formater.ex
@@ -1,2 +1,10 @@
 defmodule DurationFormater do
+  @duration_name %{
+    now: "now",
+    second: "second"
+  }
+
+  def format_duration(0), do: @duration_name.now
+  def format_duration(1), do: "1 #{@duration_name.second}"
+  def format_duration(seconds), do: "#{seconds} #{@duration_name.second}s"
 end
--- iter_1_elixir/duration_formater_test.exs
+++ iter_1_elixir/duration_formater_test.exs
@@ -2,6 +2,13 @@ Code.load_file("duration_formater.ex", __DIR__)
 ExUnit.start()
 
 defmodule DurationFormaterTest do
-  ExUnit.Case
+  use ExUnit.Case
+  import DurationFormater, only: [format_duration: 1]
+
+  test "format durations below 6 seconds" do
+    assert format_duration(0) == "now" 
+    assert format_duration(1) == "1 second"
+    assert format_duration(3) == "3 seconds"
+  end
 
 end
