174aeb29ed8a3f6b99a0fbccc8f1b5ad0d91147a
elalaouifaris <elalaouifaris@gmail.com>
2019-07-15 18:02:29 +0200

    Separate display from business logic no use of label in decompose

--- iter_1_elixir/duration_formater.ex
+++ iter_1_elixir/duration_formater.ex
@@ -24,32 +24,29 @@ defmodule DurationFormater do
 
   def format_duration(0), do: @label.now
   def format_duration(seconds) do
-    decompose_duration(seconds, @duration_types)
+    seconds
+    |> decompose_duration(@duration_types)
     |> format_output()
   end
 
   defp decompose_duration(0, _types), do: []
   defp decompose_duration(seconds, [duration_type | rest_of_types]) do
-    main_part = div(seconds, @in_seconds[duration_type]) 
-    rest_part = rem(seconds, @in_seconds[duration_type])
+    type_count = div(seconds, @in_seconds[duration_type]) 
+    rest_count = rem(seconds, @in_seconds[duration_type])
 
     cond do
-      main_part == 0 ->
-        decompose_duration(rest_part, rest_of_types)
+      type_count == 0 ->
+        decompose_duration(rest_count, rest_of_types)
 
-      main_part == 1 ->
-        [{@label.unit, @label[duration_type]} |
-          decompose_duration(rest_part, rest_of_types)]
-
-      main_part > 1 ->
-        [{main_part, @label[duration_type], @label.plural} |
-          decompose_duration(rest_part, rest_of_types)]
+      type_count >= 1 ->
+        [{duration_type, type_count} |
+          decompose_duration(rest_count, rest_of_types)]
     end
   end
 
 
-  defp format_output([{count, label}| []]), do: "#{count} #{label}"
-  defp format_output([{count, label, plural}| []]), do: "#{count} #{label}#{plural}"
+  defp format_output([{duration_type, 1}| []]), do: "#{@label.unit} #{@label[duration_type]}"
+  defp format_output([{duration_type, count}| []]), do: "#{count} #{@label[duration_type]}#{@label.plural}"
   defp format_output([before_last, last | []]) do
     format_output([before_last]) <> @label.and <> format_output([last])
   end
