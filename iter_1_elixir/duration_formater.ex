ec58f16e075fc7b25f1355d3c05ce1684fb730ff
elalaouifaris <elalaouifaris@gmail.com>
2019-07-15 16:30:10 +0200

    refactor to use duration types as input

--- iter_1_elixir/duration_formater.ex
+++ iter_1_elixir/duration_formater.ex
@@ -9,36 +9,37 @@ defmodule DurationFormater do
   }
 
   @in_seconds %{
-    minute: 60
+    minute: 60,
+    second: 1
   }
+
+  @duration_types [:minute, :second]
 
   def format_duration(0), do: @label.now
   def format_duration(seconds) do
-    decompose_duration(seconds)
+    decompose_duration(seconds, @duration_types)
     |> format_output()
   end
 
-  defp decompose_duration(seconds) do
-    minute_part = div(seconds, @in_seconds.minute) 
-    second_part = rem(seconds, @in_seconds.minute)
+  defp decompose_duration(0, _types), do: []
+  defp decompose_duration(seconds, [duration_type | rest_of_types]) do
+    main_part = div(seconds, @in_seconds[duration_type]) 
+    rest_part = rem(seconds, @in_seconds[duration_type])
 
     cond do
-      seconds == 0 ->
-        []
+      main_part == 0 ->
+        decompose_duration(rest_part, rest_of_types)
 
-      seconds == 1 ->
-        [{@label.unit, @label.second}]
+      main_part == 1 ->
+        [{@label.unit, @label[duration_type]} |
+          decompose_duration(rest_part, rest_of_types)]
 
-      seconds == @in_seconds.minute ->
-        [{@label.unit, @label.minute} | decompose_duration(second_part)]
-
-      seconds > @in_seconds.minute ->
-        [{minute_part, @label.minute, @label.plural} | decompose_duration(second_part)]
-
-      true ->
-        [{seconds, @label.second, @label.plural}]
+      main_part > 1 ->
+        [{main_part, @label[duration_type], @label.plural} |
+          decompose_duration(rest_part, rest_of_types)]
     end
   end
+
 
   defp format_output([before_last, last | []]) do
     format_output([before_last]) <> @label.and <> format_output([last])
