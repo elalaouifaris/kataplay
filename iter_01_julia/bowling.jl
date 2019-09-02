4986948eca8fab9dad7194df2467f949c171977a
elalaouifaris <elalaouifaris@gmail.com>
2019-09-02 07:31:46 +0200

    Revert "refactor score logic with no bonus"
    This reverts commit 58d35540dd3dc58638658cea3d28013d3f5aecb5.

--- iter_01_julia/bowling.jl
+++ iter_01_julia/bowling.jl
@@ -4,25 +4,17 @@ module Bowling
     "frame_separator"=> "|",
     "null_roll" => "-"
   )
-
   function score(game)
-    frame_list, bonus = _parse_frames(game)
-    frame_scores = map(_score_single_frame, frame_list)
-    score = reduce(+, frame_scores)
-    score
-  end
-
-  function _parse_frames(game)
     game = replace(game, tag_for["null_roll"] => "0")
     frames, bonus = split(game, tag_for["bonus_separator"])
     frame_list = split(frames, tag_for["frame_separator"])
 
-    frame_list, bonus
-  end
-
-  function _score_single_frame(frame)
+    score = 0
+    for frame in frame_list
       first_roll, second_roll = frame
-      score = parse(Int, first_roll) + parse(Int, second_roll)
-      score
+      score += parse(Int, first_roll) + parse(Int, second_roll)
+    end
+
+    score
   end
 end
