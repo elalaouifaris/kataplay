c09656bd7abbf87d063f43c359bf4aa1d8516df7
elalaouifaris <elalaouifaris@gmail.com>
2019-09-03 00:10:33 +0200

    Implement spares

--- iter_01_julia/bowling.jl
+++ iter_01_julia/bowling.jl
@@ -2,19 +2,53 @@ module Bowling
   tag_for = Dict(
     "bonus_separator" => "||",
     "frame_separator"=> "|",
-    "null_roll" => "-"
+    "null_roll" => '-',
+    "spare_roll" => '/'
   )
+
+  frame_full_score = 10
+
   function score(game)
+    frame_list, bonus = _parse_frames(game)
+    the_score = _score(frame_list, bonus)
+
+    the_score
+  end
+
+  function _parse_frames(game)
     game = replace(game, tag_for["null_roll"] => "0")
     frames, bonus = split(game, tag_for["bonus_separator"])
     frame_list = split(frames, tag_for["frame_separator"])
 
+    frame_list, bonus
+  end
+
+  function _score(frame_list, bonus)
     score = 0
+    add_next_roll = 0
+
     for frame in frame_list
-      first_roll, second_roll = frame
-      score += parse(Int, first_roll) + parse(Int, second_roll)
+      score, add_next_roll = _update_score(frame, score, add_next_roll)
     end
 
     score
+  end
+
+  function _update_score(frame, score, add_next_roll)
+      first_roll, second_roll = frame
+
+      if add_next_roll != 0
+        score += add_next_roll * parse(Int, first_roll)
+        add_next_roll = 0
+      end
+
+      if second_roll == tag_for["spare_roll"]
+        score += frame_full_score
+        add_next_roll += 1
+      else
+        score += parse(Int, first_roll) + parse(Int, second_roll)
+      end
+
+    score, add_next_roll
   end
 end
--- iter_01_julia/test_bowling.jl
+++ iter_01_julia/test_bowling.jl
@@ -7,6 +7,11 @@ function check_score(game, score)
 end
 
 @testset "Score evaluation without optional rolls" begin
-  @test check_score("11|11|11|11|11|11|11|11|11|11||", 20)
-  @test check_score("-1|11|11|11|11|11|11|11|11|11||", 19)
+  @test Bowling.score("11|11|11|11|11|11|11|11|11|11||") == 20
+  @test Bowling.score("-1|11|11|11|11|11|11|11|11|11||") == 19
+end
+
+@testset "Score evaluation spare rolls" begin
+  @test Bowling.score("1/|11|11|11|11|11|11|11|11|11||") == 29
+  @test Bowling.score("1/|2/|11|11|11|11|11|11|11|11||") == 39
 end
